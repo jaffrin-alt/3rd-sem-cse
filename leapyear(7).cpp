51c07160769f588f5ab780bd6cf0d0ae12890b55
jaffrin-alt <jaffrinhussain26@gmail.com>
2025-07-30 23:57:41 -0700

    Add files via upload

---
+++ leapyear(7).cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+ {
+    int y;
+    printf("Enter a year: ");
+    scanf("%d", &y);
+
+    if ((y % 4 == 0 && y % 100 != 0) || y % 400 == 0)
+        printf("Leap year\n");
+    else
+        printf("Not a leap year\n");
+
+    return 0;
+}
+
